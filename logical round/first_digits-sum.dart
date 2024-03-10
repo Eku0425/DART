e4d3aa05e4dc6013d86fa20a8169de5dfd7b60aa
Ekta Trivedi <149374328+Eku0425@users.noreply.github.com>
2024-03-10 22:18:55 +0530

    Add files via upload

---
+++ logical round/first_digits-sum.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+void main()
+{
+  stdout.write("enter the value of n:");
+  int a=int.parse(stdin.readLineSync()!);
+  int n=a;
+  int count=0;
+  int last=n % 10;
+  while(n>9)
+  {
+    n=n~/10;
+    count++;
+  }
+  print("first and last digit sum is:${n+last}");
+}
---
+++ logical round/second_larg_num.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+void main()
+{
+  List <int>l1=[];
+  var n;
+  stdout.write("enter the value of list size:");
+  n=int.parse(stdin.readLineSync()!);
+
+  var i;
+  var num;
+  for(i=0;i<n;i++)
+  {
+    stdout.write("entr the elements:a[$i]");
+    num=int.parse(stdin.readLineSync()!);
+    l1.add(num);
+  }
+  var sec_max=0;
+  for(i=0;i<n-1;i++)
+  {
+   if(l1[i]>= sec_max)
+   {
+     sec_max=l1[i];
+   }
+  
+  }
+  print("$sec_max");
+}
